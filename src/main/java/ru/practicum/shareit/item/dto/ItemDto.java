9d585b650ff5f1a624614a49d386ac9d235665a4
m.furtov <mail5692@mail.ru>
2024-02-17 17:04:21 +0300

    реализовал модель данных

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,24 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.Data;
+import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class ItemDto {
+    private String name;
+    private String description;
+    private Boolean available;
+    private User owner;
+    private ItemRequest request;
+
+    public ItemDto(String name, String description, Boolean available, ItemRequest request) {
+        this.name = name;
+        this.description = description;
+        this.available = available;
+        this.request = request;
+    }
 }
