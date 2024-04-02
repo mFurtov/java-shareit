c43c379134d270c7aa11bfbf7d4f691eb14a27e5
m.furtov <mail5692@mail.ru>
2024-04-03 00:42:09 +0300

    перед докером

--- server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -36,4 +36,16 @@ public class ErrorHandler {
         e.printStackTrace();
         return new ErrorResponse("Произошла непредвиденная ошибка.");
     }
+
+    class ErrorResponse {
+        private final String error;
+
+        public ErrorResponse(String error) {
+            this.error = error;
+        }
+
+        public String getError() {
+            return error;
+        }
+    }
 }
